9a5d13a71f39f595671ad476d0dda5bcc935da07
Vitaliya <vitka724@gmail.com>
2020-11-29 22:10:05 +0300

    commit_2

--- src/main/java/org/ilyushenko/lab4/controller/HomeController.java
+++ src/main/java/org/ilyushenko/lab4/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.ilyushenko.lab4.controller;
 
+import org.ilyushenko.lab4.dao.UserDAO;
 import org.ilyushenko.lab4.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,35 +12,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Controller
 public class HomeController {
-    Collection<User> users = new ArrayList<>();
-    @GetMapping(value = "/")
-    public String Hello() {
+    //@Autowired
+    //private UserDAO userDAO;
+
+    @GetMapping("/")
+    public String hello() {
+
         return "/Hello";
     }
     @RequestMapping(value = "/users")
-    public String getUsers(Model model)  {
-        model.addAttribute( "users", users);;
+    public String getUsers(Model model) throws SQLException {
+        //model.addAttribute( "users", userDAO.getAll() );
+        model.addAttribute( "users", null );
+
         return "Users";
     }
     @GetMapping(value = "/addUsers")
     public String getSignUp(Model model)
     {
         model.addAttribute( "user", new User(  ) );
-        return "/Sign_Up";
+        return "/Sign_up";
     }
     @PostMapping(value = "/addUsers")
     public String getSignUp(@ModelAttribute @Valid User user, BindingResult result){
         if (result.hasErrors()){
-            return "/Sign_Up";
+            return "/Sign_up";
         }
-        users.add(user);
-        return "Users";
+        //users.add(user);
+        return "redirect:/users";
     }
 }
+
 
 
---
+++ src/main/java/org/ilyushenko/lab4/dao/UserDAO.java
@@ -0,0 +1,52 @@
+package org.ilyushenko.lab4.dao;
+
+import org.ilyushenko.lab4.model.User;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+@Component
+public class UserDAO {
+    private static Connection conn;
+
+    static {
+        String url = null;
+        String username = null;
+        String password = null;
+        //load db properties
+        try (InputStream in = UserDAO.class.getClassLoader().getResourceAsStream("persistens.properties")) {
+            Properties properties = new Properties();
+            properties.load( in );
+            url = properties.getProperty("url");
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //acquire db connection
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException | ClassNotFoundException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    public List<User> getAll() throws SQLException{
+        List<User> users = new ArrayList<>();
+        PreparedStatement ps = conn.prepareStatement("select * from users" );
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            User user = new User();
+            user.setName( rs.getString(1) );
+            user.setSurname( rs.getString(2) );
+            user.setSurname( rs.getString(3) );
+            users.add( user );
+        }
+        return users;
+    }
+}
